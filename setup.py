3d1aab0a83793a44a16975d8049a624ff1235bb8
Matthieu Dorier <mdorier@anl.gov>
2020-09-28 05:00:06 -0500

    using get_include from pybind11

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ import os
 import os.path
 import sys
 
-def get_pybind11_include():
-    path = os.path.dirname(pybind11.__file__)
-    return '/'.join(path.split('/')[0:-4] + ['include'])
-
 (opt,) = get_config_vars('OPT')
 os.environ['OPT'] = " ".join(flag for flag in opt.split() if flag != '-Wstrict-prototypes')
 
@@ -28,7 +24,7 @@ sonata_server_ext = Extension('_sonata_server',
         ['pysonata/src/server_module.cpp'],
         libraries=sonata_server['libraries'],
         library_dirs=sonata_server['library_dirs'],
-        include_dirs=sonata_server['include_dirs'] + [get_pybind11_include()],
+        include_dirs=sonata_server['include_dirs'] + [pybind11.get_include()],
         language='c++14',
         extra_compile_args=extra_compile_args,
         depends=[])
@@ -37,7 +33,7 @@ sonata_admin_ext = Extension('_sonata_admin',
         ['pysonata/src/admin_module.cpp'],
         libraries=sonata_admin['libraries'],
         library_dirs=sonata_admin['library_dirs'],
-        include_dirs=sonata_admin['include_dirs'] + [get_pybind11_include()],
+        include_dirs=sonata_admin['include_dirs'] + [pybind11.get_include()],
         language='c++14',
         extra_compile_args=extra_compile_args,
         depends=[])
@@ -46,7 +42,7 @@ sonata_client_ext = Extension('_sonata_client',
         ['pysonata/src/client_module.cpp'],
         libraries=sonata_client['libraries'],
         library_dirs=sonata_client['library_dirs'],
-        include_dirs=sonata_client['include_dirs'] + [get_pybind11_include()],
+        include_dirs=sonata_client['include_dirs'] + [pybind11.get_include()],
         language='c++14',
         extra_compile_args=extra_compile_args,
         depends=[])
